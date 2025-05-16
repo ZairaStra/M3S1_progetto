e6db1260f0cbbabf5f0ccadcc3a210924ac7148c
ZairaStra <straticozaira@gmail.com>
2025-05-16 12:08:22 +0200

    Headings

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./components/MyNav";
+import "./components/Headings";
 import "./components/MyFooter";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
+import Headings from "./components/Headings";
 
 const App = function () {
   return (
     <>
       <MyNav />
-
+      <Headings />
       <MyFooter />
     </>
   );
